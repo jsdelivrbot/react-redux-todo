68e0e6414d3789fa6e0168555cbb322047abbb65
kjng <pianistkevinjang@gmail.com>
2017-04-13 07:43:58 -0700

    Create TodoList component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import TodoList from './TodoList';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <TodoList />
     );
   }
 }
