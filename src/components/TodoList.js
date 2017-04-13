5d4544a7d7f500c27001c6194db503138bff111e
kjng <pianistkevinjang@gmail.com>
2017-04-13 07:50:48 -0700

    Create TodoList form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 export default class TodoList extends Component {
   render() {
     return (
-      <div>Todo List</div>
+      <div>
+        <form>
+          <div>Todo List</div>
+          <div className="form-group">
+            <label>Add a todo:</label>
+            <input type="text" className="form-control" />
+          </div>
+          <button type="submit" className="btn btn-primary">Add</button>
+        </form>
+      </div>
     );
   }
 }
